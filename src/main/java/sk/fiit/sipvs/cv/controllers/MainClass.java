b6337d20350909a56a4602258f002d09aee15bc0
Michal Polko <michal.polko@gmail.com>
2015-10-09 18:28:24 +0200

    Added main menu + create XML form

--- src/main/java/sk/fiit/sipvs/cv/controllers/MainClass.java
+++ src/main/java/sk/fiit/sipvs/cv/controllers/MainClass.java
@@ -1,14 +1,45 @@
 package sk.fiit.sipvs.cv.controllers;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+import sk.fiit.sipvs.cv.form.FormView;
+
 public class MainClass {
 
+	private static FormView formView;
+	
 	public static void main(String[] args) throws IOException {
-		System.out.println("Hello world!");
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {}
+
+		formView = new FormView();
+		
+		JFrame window = new JFrame();
+		window.setTitle("SIPVS");
+		window.setBounds(100, 100, 280, 400);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setLayout(null);
+		window.setVisible(true);
+
+		JButton formButton = getButton("Create XML", 0);
+		window.add(formButton);
+		formButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				formView.show();
+			}
+		});	
+
+		/*System.out.println("Hello world!");
 
 		InputStream is = ClassLoader.getSystemResourceAsStream("valid_example.xml");
 		BufferedReader br = null;
@@ -24,8 +55,14 @@ public class MainClass {
 			br.close();
 		}
 
-		System.out.println(sb.toString());
-
+		System.out.println(sb.toString());*/
 	}
 
+	private static JButton getButton(String name, int position) {
+		JButton button = new JButton(name);
+		button.setBounds(32, 32 + position * 48, 200, 40);
+
+		return button;
+	}
+	
 }
---
+++ src/main/java/sk/fiit/sipvs/cv/form/Attribute.java
@@ -0,0 +1,61 @@
+package sk.fiit.sipvs.cv.form;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class Attribute {
+
+	private String name;
+	private String defaultValue;
+	private Map<String, String> values;
+	private JComboBox<String> comboBox;
+	
+	public Attribute(String name, String defaultValue) {
+		this.name = name;
+		this.defaultValue = defaultValue;
+		this.values = new HashMap<String, String>();
+	}
+	
+	public void add(String value, String title) {
+		this.values.put(value, title);
+	}
+	
+	public int createWidgets(JComponent parent, int depth, int index) {
+		comboBox = new JComboBox<String>();
+		for (String str : this.values.values()) {
+			comboBox.addItem(str);
+		}
+		this.setBounds(comboBox, depth, index, 200, 100);
+		parent.add(comboBox);
+		
+		return index;
+	}
+	
+	public void setDefaultValues() {
+		comboBox.setSelectedItem(this.values.get(this.defaultValue));
+	}
+	
+	public void getXML(Document doc, Element parentNode) {
+		Attr attr = doc.createAttribute(this.name);
+		
+		String selectedItem = (String) this.comboBox.getSelectedItem();
+		for (String str : this.values.keySet()) {
+			if (this.values.get(str) == selectedItem) {
+				attr.setValue(str);
+				parentNode.setAttributeNode(attr);	
+			}
+		}
+	}
+
+	private void setBounds(JComponent component, int depth, int index, int width, int xoffset) {
+		component.setBounds(32 + depth * 32 + xoffset, 20 + index * (24 + 2), width, 24);
+	}
+	
+}
