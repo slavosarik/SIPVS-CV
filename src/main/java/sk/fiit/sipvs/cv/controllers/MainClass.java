d72b605c96e7eb5fcf7d532c87a4cad1b3557951
slavosarik <slavosarik@gmail.com>
2015-10-09 07:53:07 +0200

    Updated project structure

--- src/main/java/sk/fiit/sipvs/cv/controllers/MainClass.java
+++ src/main/java/sk/fiit/sipvs/cv/controllers/MainClass.java
@@ -1,9 +1,30 @@
 package sk.fiit.sipvs.cv.controllers;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class MainClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("Hello world!");
+
+		InputStream is = ClassLoader.getSystemResourceAsStream("valid_example.xml");
+		BufferedReader br = null;
+		StringBuilder sb = new StringBuilder();
+		String line;
+
+		br = new BufferedReader(new InputStreamReader(is));
+		while ((line = br.readLine()) != null) {
+			sb.append(line);
+		}
+
+		if (br != null) {
+			br.close();
+		}
+
+		System.out.println(sb.toString());
 
 	}
 
